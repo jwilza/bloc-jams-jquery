86141c0c2798a82e84706d4178ad2081c174b5f2
wilza <wilza@example.com>
2020-08-27 18:45:29 -0400

    Checkpoint 17 Completed

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -4,10 +4,33 @@ const album = {
   releaseInfo: '1909 Spanish Records',
   albumArtUrl: 'assets/images/album_covers/01.png',
   songs: [
-    { title: 'Blue', duration: '161.71', soundFileUrl: 'assets/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', soundFileUrl: 'assets/music/green.mp3' },
+    {
+      title: 'Blue',
+      duration: '161.71',
+      soundFileUrl: 'assets/music/blue.mp3'
+    },
+    {
+      title: 'Green',
+      duration: '103.96',
+      soundFileUrl: 'assets/music/green.mp3'
+    },
     { title: 'Red', duration: '268.45', soundFileUrl: 'assets/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', soundFileUrl: 'assets/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', soundFileUrl: 'assets/music/magenta.mp3' }
+    {
+      title: 'Pink',
+      duration: '153.14',
+      soundFileUrl: 'assets/music/pink.mp3'
+    },
+    {
+      title: 'Magenta',
+      duration: '374.22',
+      soundFileUrl: 'assets/music/magenta.mp3'
+    }
   ]
-};
+}
+
+{
+  $('#album-title').text(album.title)
+  $('img#album-cover-art').attr('src', album.albumArtUrl)
+  $('#artist').text(album.artist)
+  $('#release-info').text(album.releaseInfo)
+}
